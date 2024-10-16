c0ead9ecd42123a048625d9e7bb210c1aecd106b
Talha Tariq <tariqtalha936@gmail.com>
2024-10-17 00:43:09 +0500

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+	fontFamily:{
+		primary:"var(--font--jetbrainsMono)",
+
+	},
+  	extend: {
+  		colors: {
+			primary:'#1c1c22',
+			accent:{
+				DEFAULT:'#00ff99',
+				hover:'#00e187'
+			},
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
